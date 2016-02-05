f936e7636d1b2d6e4928d1454400079fc26fe286
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-02-05 22:33:42 +0000

    replicas

--- public/ajax/encode_post.php
+++ public/ajax/encode_post.php
@@ -33,10 +33,10 @@ foreach ($json as $json_object) {
 	if(isset($json_object['duration'])){
 		$json_object['duration'] = intval($json_object['duration']);
 	}
-	if(isset($json[$count]['biological_replicate_number'])){
+	if(isset($json_object['biological_replicate_number'])){
 		$json_object['biological_replicate_number'] = intval($json_object['biological_replicate_number']);
 	}
-	if(isset($json[$count]['technical_replicate_number'])){
+	if(isset($json_object['technical_replicate_number'])){
 		$json_object['technical_replicate_number'] = intval($json_object['technical_replicate_number']);
 	}
 	#POST the JSON and get back response
