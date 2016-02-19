43d83879541e41f0c2cec7c8f5b0103c9feaf804
Nick Merowsky <nick.merowsky@umassmed.edu>
2016-02-19 18:53:52 +0000

    @@upload

--- public/ajax/encode_files.php
+++ public/ajax/encode_files.php
@@ -355,7 +355,6 @@ foreach($file_query as $fq){
 					}
 				}
 				$response = Requests::patch($url, $headers, json_encode($data), $auth);
-				$response = Requests::get($url."@@upload", $headers, $auth);
 				$body = json_decode($response->body);
 			}
 			
