ca4bcd38fce344b94b8c006c20dd20dc07915a8a
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-04-07 21:57:34 +0000

    import issues

--- public/ajax/ngsquerydb.php
+++ public/ajax/ngsquerydb.php
@@ -320,20 +320,20 @@ else if ($p == 'getCustomTSV')
 else if ($p == 'checkOutputDir')
 {
 	if (isset($_GET['outdir'])){$outdir = $_GET['outdir'];}
-	$data=$query->queryAVal("
+	$data=json_decode($query->queryAVal("
     SELECT outdir
     FROM ngs_runparams
     where outdir = '$outdir'
-    ");
+    "));
 }
 else if ($p == 'checkRunID')
 {
 	if (isset($_GET['outdir'])){$outdir = $_GET['outdir'];}
-	$data=$query->queryAVal("
+	$data=json_decode($query->queryAVal("
     SELECT run_group_id
     FROM ngs_runparams
     where outdir = '$outdir'
-    ");
+    "));
 }
 else if ($p == 'changeDataGroupNames')
 {
