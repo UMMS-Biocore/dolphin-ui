79aa5b783238247004306bcfb360a434e51512f6
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-06-03 22:11:37 +0000

    file fix

--- scripts/dolphin_wrapper.py
+++ scripts/dolphin_wrapper.py
@@ -603,7 +603,6 @@ class Dolphin:
                 type="haplotypecaller"
 
         level = str(1 if ('clean' in runparams and runparams['clean'].lower() != 'none') else 0)
-        print >>fp, '%s'%(stepSummary % locals())
         print >>fp, '%s'%(stepClean % locals())
 
         fp.close()
