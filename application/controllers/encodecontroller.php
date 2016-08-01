e353fd8fe52943ec8a639266d8bbd05c0019b579
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-08-01 14:05:18 +0000

    encode page creation

---
+++ application/controllers/encodecontroller.php
@@ -0,0 +1,17 @@
+<?php
+
+class EncodeController extends VanillaController {
+
+	function beforeAction() {
+        
+	}
+	
+	function index() {
+		$this->set('field', "Encode");
+	}
+	
+	function afterAction() {
+        
+	}
+
+}
---
+++ application/views/encode/index.php
@@ -0,0 +1,11 @@
+				<section class="content-header">
+					<h1>
+						Encode Viewing/Submission
+						<small>Projects and experiments submitted to Encode</small>
+					</h1>
+					<ol class="breadcrumb">
+						<li><a href="<?php echo BASE_PATH?>"><i class="fa fa-dashboard"></i> Home</a></li>
+						<li><a href="<?php echo BASE_PATH."/search"?>">NGS Browser</a></li>
+						<li class="active"><?php echo $field?></li>
+					</ol>
+				</section>
