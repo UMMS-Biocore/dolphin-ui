91b572f12ab88fbaaf65198c9dbc907bddbd5a0f
Nick Merowsky <nicholas.merowsky@umassmed.edu>
2016-09-23 19:03:44 +0000

    docker issue with import resolved

--- application/models/ngsimport.php
+++ application/models/ngsimport.php
@@ -1,9 +1,4 @@
 <?php
-if (!headers_sent()) {
-    header('Cache-Control: no-cache, must-revalidate');
-    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-    header('Content-type: application/json');
-}
 error_reporting(E_ALL);
 ini_set('report_errors','on');
 
