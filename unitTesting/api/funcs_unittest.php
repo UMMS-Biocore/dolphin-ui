60837fd288d6c0a816ef18d55c7c2b7c04eacf98
Nick Merowsky <nmerowsky@gmail.com>
2016-12-20 11:38:11 -0500

    unit test fixes

--- unitTesting/api/funcs_unittest.php
+++ unitTesting/api/funcs_unittest.php
@@ -423,7 +423,7 @@ class funcs_unittest extends PHPUnit_Framework_TestCase
 	public function testGetAmazonCredentials(){
 		ob_start();
 		$funcs  = new funcs();
-        $params['username'] = 'kucukura';
+        $params['username'] = 'docker';
 		$this->assertEquals($funcs->getAmazonCredentials($params)[0]['id'], '1');
 		$this->assertEquals($funcs->getAmazonCredentials($params)[0]['bucket'], 'test_bucket');
 		ob_end_clean();
